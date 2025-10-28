2825ba1d22069fecdbc892513406de5101cdc2de
Halimah Akinoso <halimahAkin2@gmail.com>
2025-10-28 14:56:16 +0100

    react 7  added

---
+++ Day_3.1/currency_converter/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import CurrencyConverter from './components/CurrencyConverter'
+import KeyPressLogger from './components/KeyPressLogger'
+import PrayerTimeTracker from './components/PrayerTimeTracker'
+
+
+const App = () => {
+  return (
+    // <div>
+    //   <CurrencyConverter />
+    // </div>
+
+    // <div>
+    //   <KeyPressLogger />
+    // </div>
+    <div>
+      <PrayerTimeTracker />
+    </div>
+  )
+}
+
+export default App
